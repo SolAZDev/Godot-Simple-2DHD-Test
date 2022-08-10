528343ac9055e375e6167c1e9334b5f7e57df11b
SolAZDev <SolAZDev@gmail.com>
2022-08-10 14:53:04 -0400

    GLES2

--- docs/index.service.worker.js
+++ docs/index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1660144213|87593543";
+const CACHE_VERSION = "1660157470|880327994";
 const CACHE_PREFIX = "HD2DTest-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
